154dd21903be26381d57a7ed59b9edcebb68ffc4
Gaben <Gaben@DESKTOP-OBPJOHA>
2020-09-10 14:20:15 -0500

    Controller para hacer debug

--- src/co/edu/unbosque/controller/Controller.java
+++ src/co/edu/unbosque/controller/Controller.java
@@ -1,9 +1,34 @@
 package co.edu.unbosque.controller;
 
+import java.util.Scanner;
+
 public class Controller {
 	
 	public void start() {
-		
+		Scanner sc = new Scanner(System.in);
+		var active = true;
+		while(active) {
+			try {
+				System.out.print("Ingresa una opcion [0|1|2] ");
+				var option = sc.nextInt();
+				switch(option) {
+				default:
+					System.out.println("ERROR: Opción inválida. Por favor ingresa las opciones disponibles.");
+					break;
+				case 0:
+					active = false;
+					sc.close();
+					break;
+				case 1:
+					break;
+				case 2:
+					break;
+				}
+			} catch (Exception e) {
+				System.out.println("ERROR: Opción inválida. Por favor ingresa las opciones disponibles.");
+				start();
+			}
+		}
 	}
 	
 	public void startClient() {
